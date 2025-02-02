1e665ebe99c086575882bae0347f59b9e97114bc
MR. Morningstar <111835338+EmonJoy@users.noreply.github.com>
2025-02-02 22:10:59 +0600

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -2,10 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Admin extends JFrame  {
 
@@ -15,6 +12,11 @@ public class Admin extends JFrame  {
     int width = (int) size.getWidth();
     int height = (int) size.getHeight();
 
+    LoginForm l1 = new LoginForm();
+
+
+    MainFrame seeSong = new MainFrame(l1.LoginUser);
+
     Admin(){
         setTitle("Profile");
         setLayout(null);
@@ -23,12 +25,49 @@ public class Admin extends JFrame  {
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        ImageIcon img = new ImageIcon(getClass().getResource("user.png"));
+        Image dp = img.getImage();
+
+        Image scaledImage = dp.getScaledInstance(180,180, Image.SCALE_SMOOTH);
+        img = new ImageIcon(scaledImage);
+
+        JLabel adminPhoto = new JLabel(img);
+        adminPhoto.setBounds(800,80,180,180);
+        add(adminPhoto);
+
+        JLabel adminLabel = new JLabel("User Profile");
+        adminLabel.setBounds(500,5,600,50);
+        adminLabel.setFont(new Font("serif", Font.BOLD, 40));
+        add(adminLabel);
+
+        JSeparator sep = new JSeparator();
+        sep.setBounds(500,58,200,2);
+        add(sep);
+
+
+
+        JLabel userInfo = new JLabel("User Info: ");
+        userInfo.setBounds(100,60,600,40);
+        userInfo.setFont(new Font("times new roman",Font.BOLD,40));
+        add(userInfo);
+
+        JLabel post_details = new JLabel("Post Details: ");
+        post_details.setFont(new Font("times new roman",Font.BOLD,40));
+        post_details.setBounds(100,340,600,40);
+        add(post_details);
+
         JTextArea j = new JTextArea();
-        j.setBounds(100, 100 , 600,600);
+        j.setBounds(100, 100 , 600,200);
         j.setFont(new Font("Times new roman",Font.BOLD, 20));
 
+
+        JTextArea j1 = new JTextArea();
+        j1.setBounds(100, 380 , 600,300);
+        j1.setFont(new Font("Times new roman",Font.BOLD, 20));
+
         JButton btn = new JButton("back");
-        btn.setBounds(800,100, 200,100);
+        btn.setBounds(100,700, 200,40);
+        btn.setBackground(new Color(204,255,204));
         btn.setFocusable(false);
         btn.addActionListener(new ActionListener() {
             @Override
@@ -41,7 +80,8 @@ public class Admin extends JFrame  {
 
         JButton logout = new JButton("Logout");
         logout.setFocusable(false);
-        logout.setBounds(800, 300 , 200,100);
+        logout.setBackground(new Color(255,153,255));
+        logout.setBounds(320, 700 , 200,40);
 
         logout.addActionListener(new ActionListener() {
             @Override
@@ -67,17 +107,40 @@ public class Admin extends JFrame  {
                 text += line +"\n";
             }
             j.setText(text);
-
-
         }catch (IOException ae)
         {
             JOptionPane.showMessageDialog(null,"Error","Error Title",JOptionPane.ERROR_MESSAGE);
         }
 
+
+
+        // read file and add file
+
+        try (BufferedReader r = new BufferedReader(new FileReader("Post.txt"))){
+            String line1;
+            String text1 = "";
+            while ((line1 = r.readLine())!=null){
+                text1 += line1 +"\n";
+            }
+            j1.setText(text1);
+
+
+        }catch (IOException ae){
+            JOptionPane.showMessageDialog(null,"Error","Error Title",JOptionPane.ERROR_MESSAGE);
+
+        }
+
+
         add(j);
+        add(j1);
 
 
         setVisible(true);
     }
+
+    public static void main(String[] args) {
+        new Admin();
+    }
+
 
 }
